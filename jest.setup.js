98b80345277759139a68808b0ca8ed0e586e3f35
Zahidul Hossain <choyan.009@gmail.com>
2022-11-09 19:48:37 +0600

    Init

---
+++ jest.setup.js
@@ -0,0 +1,19 @@
+import '@testing-library/jest-dom'
+import 'regenerator-runtime/runtime'
+import '@testing-library/jest-dom/extend-expect'
+
+window.matchMedia = (query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+})
+
+Object.defineProperty(URL, 'createObjectURL', {
+    writable: true,
+    value: jest.fn(),
+})
