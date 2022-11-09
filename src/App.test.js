75f60c32489c5da6dff04eb2a1c882a4d8354867
Zahidul Hossain <choyan.009@gmail.com>
2022-11-09 23:37:41 +0600

    Simple test implementation

---
+++ src/App.test.js
@@ -0,0 +1,7 @@
+import { render, screen } from 'utils/test-utils'
+import { App } from './App'
+
+test('Renders App Component', () => {
+    render(<App />)
+    expect(screen.getByRole('heading')).toHaveTextContent('Hello world!')
+})
